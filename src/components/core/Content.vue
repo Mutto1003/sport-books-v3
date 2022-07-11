50ec2c1073825af73729557eae9a870d37c5167a
Panithan Bunrong <Panithan.bunrong@gmail.com>
2022-07-11 10:38:38 +0700

    Update Dashboard & Table

--- src/components/core/Content.vue
+++ src/components/core/Content.vue
@@ -1,20 +1,27 @@
 <template>
   <a-layout-content
     :style="{
-      margin: '24px 16px',
+      margin: '10px 16px',
       padding: '24px',
       background: '#fff',
       minHeight: '280px',
     }"
   >
-    
+    <CorrectScoreTable />
   </a-layout-content>
 </template>
 
 <script>
+import CorrectScoreTable from "@/components/table/CorrectScoreTable.vue";
+import { LeftCircleFilled, RightCircleFilled } from "@ant-design/icons-vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  components: {
+    LeftCircleFilled,
+    RightCircleFilled,
+    CorrectScoreTable,
+  },
   setup() {},
 });
 </script>
---
+++ src/components/table/CorrectScoreTable.vue
@@ -0,0 +1,75 @@
+<template>
+  <a-table :columns="columns" :data-source="data" bordered>
+    <template #bodyCell="{ column, text }">
+      <template v-if="column.dataIndex === 'name'">
+        <a>{{ text }}</a>
+      </template>
+    </template>
+    <template #title>ออสเตรเลีย ฟุตบอล ควีนส์แลนด์ พรีเมียร์ลีก 2</template>
+  </a-table>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+const columns = [
+  {
+    title: "รายการ",
+    dataIndex: "name",
+  },
+  {
+    title: "0-1",
+    className: "column-money",
+    dataIndex: "money",
+  },
+  {
+    title: "0-2",
+    dataIndex: "address",
+  },
+  {
+    title: "0-2",
+    dataIndex: "address",
+  },
+  {
+    title: "0-2",
+    dataIndex: "address",
+  },
+  {
+    title: "0-2",
+    dataIndex: "address",
+  },
+  {
+    title: "0-2",
+    dataIndex: "address",
+  },
+  {
+    title: "0-2",
+    dataIndex: "address",
+  },
+  {
+    title: "0-2",
+    dataIndex: "address",
+  },
+  {
+    title: "0-2",
+    dataIndex: "address",
+  },
+];
+const data = [
+  {
+    key: "1",
+    name: "John Brown VS John Brown",
+    money: "",
+    address: "",
+  },  
+];
+
+export default defineComponent({
+  setup() {
+    return { data, columns };
+  },
+});
+</script>
+
+<style scoped>
+@import "@/css/styles.css";
+</style>
