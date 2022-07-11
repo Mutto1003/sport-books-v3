0a9c0578ccc55641fa4e40d56bd5da3bcfce3fd7
Panithan Bunrong <Panithan.bunrong@gmail.com>
2022-07-11 19:35:21 +0700

    Update Content v2
    เพิ่มโฟเดอร์ pages

--- src/components/core/Content.vue
+++ src/components/core/Content.vue
@@ -7,41 +7,17 @@
       minHeight: '280px',
     }"
   >
-    <a-row type="flex">
-      <a-col flex="1 1 200px">
-        <Live />
-        <br />
-        <VariouRresults />
-        <br />
-        <CorrectScoreTable />
-      </a-col>
-      <a-col
-        flex="0 1 300px"
-        :style="{
-          margin: '0px 0px 0px 16px',
-        }"
-        ><FootballProgram
-      /></a-col>
-    </a-row>
+    <SportBet />
   </a-layout-content>
 </template>
 
 <script>
-import CorrectScoreTable from "@/components/table/CorrectScoreTable.vue";
-import Live from "@/components/live/Live.vue";
-import VariouRresults from "@/components/results/VariouRresults.vue";
-import FootballProgram from "@/components/cards/FootballProgram.vue";
-import { LeftCircleFilled, RightCircleFilled } from "@ant-design/icons-vue";
+import SportBet from "@/components/pages/SportBet.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   components: {
-    LeftCircleFilled,
-    RightCircleFilled,
-    CorrectScoreTable,
-    Live,
-    VariouRresults,
-    FootballProgram,
+    SportBet,
   },
   setup() {},
 });
---
+++ src/components/pages/SportBet.vue
@@ -0,0 +1,42 @@
+<template>
+  <a-row type="flex">
+    <a-col flex="1 1 200px">
+      <Live />
+      <br />
+      <VariouRresults />
+      <br />
+      <CorrectScoreTable />
+    </a-col>
+    <a-col
+      flex="0 1 300px"
+      :style="{
+        margin: '0px 0px 0px 16px',
+      }"
+      ><FootballProgram
+    /></a-col>
+  </a-row>
+</template>
+
+<script>
+import CorrectScoreTable from "@/components/table/CorrectScoreTable.vue";
+import Live from "@/components/live/Live.vue";
+import VariouRresults from "@/components/results/VariouRresults.vue";
+import FootballProgram from "@/components/cards/FootballProgram.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    CorrectScoreTable,
+    Live,
+    VariouRresults,
+    FootballProgram,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+@import "@/css/styles.css";
+</style>
